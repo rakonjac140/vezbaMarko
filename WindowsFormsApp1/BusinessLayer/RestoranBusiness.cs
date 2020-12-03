61dc8af07b652cb9537089981187243b2632d6c9
DESKTOP-PR30UJ0\Rakonjac <srakonjac140@gmail.com>
2020-12-03 17:13:19 +0100

    no message

---
+++ WindowsFormsApp1/BusinessLayer/RestoranBusiness.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class RestoranBusiness
+    {
+        private readonly RestoranRepository restoranRepository;
+
+        public List<Restoran> GetAllMenuItems()
+        {
+            return this.restoranRepository.GetAllMenuItems();
+        }
+
+        public bool InsertItem(Restoran r)
+        {
+            if (this.restoranRepository.InsertMenuItem(r) > 0)
+                return true;
+            return false;
+        }
+
+        public List<Restoran> GetStudentsBetween(double min, double max)
+        {
+            this.restoranRepository.GetAllMenuItems().Where( r => Convert.ToDouble(r.Price) > min && Convert.ToDouble(r.Price) < max).ToList();
+        }
+
+
+    }
+
+
+}
---
+++ WindowsFormsApp1/DataAccessLayer/RestoranRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class RestoranRepository
+    {
+        public List<Restoran> GetAllMenuItems()
+        {
+            List<Restoran> results = new List<Restoran>();
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Students";
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while(sqlDataReader.Read())
+                {
+                    Restoran r = new Restoran();
+                    r.Id = sqlDataReader.GetInt32(0);
+                    r.Title = sqlDataReader.GetString(1);
+                    r.Description = sqlDataReader.GetString(2);
+                    r.Price = sqlDataReader.GetDecimal(3);              
+                }
+                return results;
+            }
+        }
+
+        public int InsertMenuItem(Restoran r)
+        {
+            using(SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "INSERT INTO MenuItems VALUES ('{0}','{1}',{2})", r.Title, r.Description, r.Price);
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
